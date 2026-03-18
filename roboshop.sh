bff869b90d1004b270d4f09ca36afd800c52ab4a
satyam7513 <satyamknaidu@gmail.com>
2026-03-18 19:58:51 +0530

    roboshop, mongo created

--- roboshop.sh
+++ roboshop.sh
@@ -29,7 +29,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
